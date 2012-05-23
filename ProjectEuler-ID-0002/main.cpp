8f931d5ed856f6fee1abeab488b79f650abc0df7
Blake Merryman <cinaed28@gmail.com>
2012-05-23 18:24:03 -0500

    Version 1
    Submitted to Project Euler

---
+++ ProjectEuler-ID-0002/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  ProjectEuler-ID-0002
+//
+//  Created by Blake Merryman on 5/12/12.
+//  Copyright (c) 2012 Cinaed Designs. All rights reserved.
+//
+//  Description:   This software was written to solve problem 2 from
+//  ProjectEuler.com. By considering the terms in the Fibonacci sequence 
+//  whose values do not exceed four million, find the sum of the even-valued 
+//  terms.
+//
+//  Outcome:    In progress...
+//  Solution:   In progress...
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+    
+    return 0;
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-ProjectEuler-0002
-=================
-
-By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
